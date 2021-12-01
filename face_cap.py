a1821252d03a473a0a2af9e0c75c1f5db336cb10
samartawfik <85593654+samartawfik@users.noreply.github.com>
2021-12-01 17:01:28 -0500

    added text for showing forehead

--- face_cap.py
+++ face_cap.py
@@ -7,9 +7,12 @@ while True:
     ret, img = cam.read()
     if ret:
         # cv2.imshow("Put your face here", img)
+        img = cv2.flip(img,1)
         ell_img = cv2.ellipse(img, (320, 240), (200, 150), 90, 0, 360, (255, 0, 0), 3)
-        cv2.putText(ell_img, "Put your face here", org=(170, 470), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(0, 0, 255), thickness=2)
-        cv2.imshow("Face here", ell_img)
+        cv2.putText(ell_img, "Put your face here", org=(170, 30), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(0, 0, 255), thickness=2)
+        cv2.putText(ell_img, "Make sure your forehead is visible", org=(50, 470), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(0, 0, 255), thickness=2)
+        cv2.imshow(" ", ell_img)
         key = cv2.waitKey(1) & 0XFF
         if key == 'q':
             break
+
