372ea53ffe3b3957a7abb3979830252fd0e12353
Jack Smith <jacktsmi@umich.edu>
2021-12-02 23:14:01 -0500

    Moved all important files to src, did a bit of renaming, and wrote stuff in OVERVIEW.txt

---
+++ src/run.sh
@@ -0,0 +1,3 @@
+sudo udevadm trigger
+python3 ir_pi_sensor_interface.py &
+python3 face_cap.py &
--- threading_bash.sh
+++
@@ -1,3 +0,0 @@
-sudo udevadm trigger
-python3 src/ir_pi_sensor_interface.py &
-python3 src/face_cap.py &
